8ee5ae1daaed1745704e25808f6aa98af8df3c13
Pan-Artur <pankovets.artur1@gmail.com>
2025-07-19 00:52:26 +0300

    creating new-posts

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,8 +1,11 @@
+import { NewPosts } from "./NewPosts/NewPosts";
+
 export const Main = () => {
 
     return (
         <>
             <h3>Main page</h3>
+            <NewPosts />
         </>
     );
 };
